9564903e3ff97eed6d0bd5273bdacc8834921d63
Tom Daniel <88054834+tomdan-ai@users.noreply.github.com>
2023-05-25 10:33:00 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,35 @@
 const Home = () => (
   <div>
-    <h2>Welcome to our page!</h2>
+    <h2>Welcome to Math Magicians - Your Ultimate Source for Quotes and Calculations!</h2>
+ 
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam quam laboriosam
-      mollitia porro, est illo aspernatur vel tenetur saepe enim dolores ducimus fugiat
-      eveniet dicta explicabo! Dolores perferendis tempora quaerat!
+     At Math Magicians, we believe in the power of numbers and the magic they can create.
+  Whether you're seeking a dose of inspiration or need to crunch some numbers, our web
+  app has got you covered.
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam quam laboriosam
-      mollitia porro, est illo aspernatur vel tenetur saepe enim dolores ducimus fugiat
-      eveniet dicta explicabo! Dolores perferendis tempora quaerat!
+     Unleash your creativity with our Quote Generator. Discover captivating quotes from renowned thinkers, writers,
+  and visionaries that will ignite your 
+  imagination and provide you with the motivation you need to conquer any challenge.
     </p>
+  <p>
+  But that's not all - we go beyond inspiration. Need to tackle complex calculations? Look no further than our intuitive Calculator. 
+  From basic arithmetic to advanced mathematical functions, 
+  our calculator will help you solve equations, crunch numbers, and make those calculations a breeze.
+  </p>
+  <p>
+  At Math Magicians, we strive for simplicity and user-friendliness. Our sleek and responsive 
+  interface ensures a seamless experience across all devices, 
+  whether you're on your desktop, tablet, or mobile phone.
+  </p>
+  <p>
+  Join our community of math enthusiasts, dreamers, and seekers of wisdom. Explore the boundless possibilities of numbers and words with Math Magicians today!
+  </p>
+  <p>
+  Are you ready to dive into the world of Math Magicians? Start generating quotes 
+  and conquering calculations now. Experience the magic firsthand – launch the app and
+  let the math wizardry begin!
+  </p>
   </div>
 );
 
